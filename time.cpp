36d43f75e3e2ab4cd93706c1d8321d77a19f1cc2
nKadykov <nkadykov@gmail.com>
2023-10-25 19:15:29 +0300

    init

---
+++ time.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Time {
+    int hour;
+    int minute;
+    int second;
+public:
+    Time() : hour(0), minute(0), second(0) {}
+    Time(int h, int m, int s) { 
+        hour = h;
+        minute = m;
+        second = s;
+    }
+    void showTime() {
+        if(hour < 10)
+            cout << '0';
+        cout << hour << ':';
+        if(minute < 10)
+            cout << '0';
+        cout << minute << ':';
+        if(second < 10)
+            cout << '0';
+        cout << second << '\n';
+    }
+    Time addTime(Time time1, Time time2) {
+        Time temp;
+        int add = 0;
+        second = time1.second + time2.second;
+        if(second > 59)
+            add = 1;
+        minute = time1.minute + time2.minute + add;
+        if(minute > 59)
+            add = 1;
+        else
+            add = 0;
+        hour = time1.hour + time2.hour + add;
+        if(hour > 23)
+            hour = hour - 24;
+        return temp;
+    }
+};
+
+int main() {
+    Time time(1, 1, 9);
+    time.showTime();
+    Time time1(13, 23, 43), time2(12, 14, 15), temp;
+    temp.addTime(time1, time2);
+    temp.showTime();
+
+    return 0;
+}
