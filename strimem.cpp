8c4ac1721de05c0f8853811bd57b868b8c25d221
nKadykov <nkadykov@gmail.com>
2023-11-21 23:07:34 +0300

    init

---
+++ strimem.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class StrCount {
+    int count;
+    char* str;
+    friend class String;
+    StrCount(char* s) {
+        int length = strlen(s);
+        str = new char[length + 1];
+        strcpy(str, s);
+        count = 1;
+    }
+    ~StrCount() {
+        delete[] str;
+    }
+};
+
+class String {
+    StrCount* psc;
+public:
+    String() {
+        psc = new StrCount("NULL");
+    }
+    String(char* s) {
+        psc = new StrCount(s);
+    }
+    String(String& S) {
+        psc = S.psc;
+        (psc->count)++;
+    }
+    ~String() {
+        if(psc->count == 1) {
+            delete psc;
+        }
+        else {
+            (psc->count)--;
+        }
+    }
+    void display() {
+        cout << psc->str;
+        cout << " (addr = " << psc << ")";
+    }
+    void operator = (String& S) {
+        if(psc->count == 1) {
+            delete psc;
+        }
+        else {
+            (psc->count)--;
+        }
+        psc = S.psc;
+        (psc->count)++;
+    }
+};
+
+int main() {
+    String s3 = "Qwerty";
+    cout << "\ns3 = ";
+    s3.display();
+
+    String s1;
+    s1 = s3;
+    cout << "\ns1 = ";
+    s1.display();
+
+    String s2(s3);
+    cout << "\ns2 = ";
+    s2.display();
+
+    return 0;
+}
