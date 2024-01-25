bbb0e72873a0b31449879ab2e62b9433ea3e6f44
nKadykov <nkadykov@gmail.com>
2024-01-25 23:22:49 +0300

    init

---
+++ 2/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << 1990 << setw(8) << 135 << '\n';
+    cout << 1991 << setw(8) << 7290 << '\n';
+    cout << 1992 << setw(8) << 11300 << '\n';
+    cout << 1993 << setw(8) << 16200 << '\n';
+
+    return 0;
+}
