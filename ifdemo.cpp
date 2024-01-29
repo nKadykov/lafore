84729553ef229f53923cc121317b70a7e089887f
nKadykov <nkadykov@gmail.com>
2024-01-29 19:25:18 +0300

    init

---
+++ ifdemo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter number: ";
+    cin >> x;
+    if(x > 100) {
+        cout << "Greater than 100\n";
+    }
+    return 0;
+}
