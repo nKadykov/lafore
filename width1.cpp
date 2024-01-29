72a35dddbdfbf0e2214547b4770f36922159b553
nKadykov <nkadykov@gmail.com>
2024-01-29 16:34:02 +0300

    init

---
+++ width1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long pop1 = 4789426;
+    long pop2 = 274124;
+    long pop3 = 9761;
+    cout << "City " << "Pop. " << endl << "Moscow " << pop1 << endl << "Kirov " << pop2 << endl << "Ugryumovka " << pop3 << endl;
+    return 0; 
+}
