fb627377848d2cd54f0cda80bc4c1d2336e076bb
nKadykov <nkadykov@gmail.com>
2024-01-29 16:51:24 +0300

    init

---
+++ mixed.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 7;
+    float avgWeight = 155.5F;
+    double totalWeight = count * avgWeight;
+    cout << "Weight is " << totalWeight << endl;
+    return 0;
+}
