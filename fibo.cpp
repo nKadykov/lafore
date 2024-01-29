8108a73c54f4fd89eaaabf91a91e27da25a298d6
nKadykov <nkadykov@gmail.com>
2024-01-29 19:02:54 +0300

    init

---
+++ fibo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const unsigned long limit = 4294967295;
+    unsigned long next = 0;
+    unsigned long last = 1;
+    while(next < limit / 2) {
+        cout << last << " ";
+        long sum = next + last;
+        next = last;
+        last = sum;
+    }
+    cout << '\n';
+    return 0;
+}
