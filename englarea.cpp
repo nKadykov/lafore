744dae3e86ec16d8dd1e101f26b1be1472d0e7a9
nKadykov <nkadykov@gmail.com>
2024-02-01 07:57:59 +0300

    init

---
+++ englarea.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Distance {
+    int feet;
+    float inches;
+};
+
+struct Room {
+    Distance length;
+    Distance width;
+};
+
+int main() {
+    Room dining;
+    dining.length.feet = 13;
+    dining.length.inches = 6.5;
+    dining.width.feet = 10;
+    dining.width.inches = 0.0;
+
+    float l = dining.length.feet + dining.length.inches / 12;
+    float w = dining.width.feet + dining.width.inches / 12;
+
+    cout << "Area of room is: " << l * w << " square meters\n";
+    
+    return 0;
+}
