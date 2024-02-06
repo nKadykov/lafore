e128173fc6ed3cdeaafbdf2e37c2cccbaa860c04
nKadykov <nkadykov@gmail.com>
2024-02-06 12:38:01 +0300

    init

--- 6/8.cpp
+++ 6/8.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 class Class {
     static int count;
+    int number = count;
 public:
     Class() {
         count++;
     }
     void getcount() {
         cout << count << endl;
+    }
+    void getnumber() {
+        cout << number << endl;
     }
 };
 
@@ -17,8 +21,11 @@ int Class::count = 0;
 int main() {
     Class o1, o2, o3;
     o1.getcount();
+    o1.getnumber();
     o2.getcount();
+    o2.getnumber();
     o3.getcount();
+    o3.getnumber();
 
     return 0;
 }
