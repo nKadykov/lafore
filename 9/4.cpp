0de5a2c3a5b9bed7fe8ab569dc6a5c4815ba2edb
nKadykov <nkadykov@gmail.com>
2024-02-16 14:18:13 +0300

    init

---
+++ 9/4.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+class Sales {
+    enum {MONTH = 3};
+    float sale[MONTH];
+public:
+    void getdata();
+    void putdata() const;
+};
+
+void Sales::getdata() {
+    cout << "Enter sales in 3 months\n";
+    for(int j = 0; j < MONTH; j++) {
+        cout << "Month " << j + 1 << ": ";
+        cin >> sale[j];
+    }
+}
+
+void Sales::putdata() const {
+    for(int i = 0; i < MONTH; i++) {
+        cout << "Sales in " << i + 1 << ": ";
+        cout << sale[i] << '\n';
+    }
+}
+
+class Publication {
+    string str;
+    float price;
+public:
+    void getdata() {
+        cout << "Enter string and price: ";
+        cin >> str >> price;
+    }
+    void putdata() const {
+        cout << "String: " << str << "\nPrice: " << price << '\n';
+    }
+};
+
+class Book : public Publication, public Sales {
+    int page;
+public:
+    void getdata() {
+        Publication::getdata();
+        cout << "Enter pages: ";
+        cin >> page;
+        Sales::getdata();
+    }
+    void putdata() const {
+        Publication::putdata();
+        cout << "Pages: " << page << '\n';
+        Sales::putdata();
+    }
+};
+
+class Type : public Publication, public Sales {
+    float time;
+public:
+    void getdata() {
+        Publication::getdata();
+        cout << "Enter time: ";
+        cin >> time;
+        Sales::getdata();
+    }
+    void putdata() {
+        Publication::putdata();
+        cout << "Time: " << time << '\n';
+        Sales::putdata();
+    }
+};
+
+class Disk : public Publication, public Sales {
+    enum type {CD, DVD};
+    type disk;
+public:
+    void getdata() {
+        char ch;
+        Publication::getdata();
+        cout << "Enter type (c oder d): ";
+        cin >> ch;
+        switch(ch) {
+            case 'c':
+                disk = CD;
+                break;
+            case 'd':
+                disk = DVD;
+                break;
+        }
+        Sales::getdata();
+    }
+    void putdata() const {
+        Publication::putdata();
+        cout << "Type: ";
+        switch(disk) {
+            case CD:
+                cout << "CD";
+                break;
+            case DVD:
+                cout << "DVD";
+                break;
+        }
+        cout << endl;
+        Sales::putdata();
+    }
+};
+
+int main() {
+    Book b;
+    Type t;
+    Disk d;
+    b.getdata();
+    b.putdata();
+    t.getdata();
+    t.putdata();
+    d.getdata();
+    d.putdata();
+    return 0;
+}
