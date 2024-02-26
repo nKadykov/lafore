88eb49cb028844e709d5a7f7120c96b7badab755
nKadykov <nkadykov@gmail.com>
2024-02-26 22:57:50 +0300

    init

---
+++ 11.cpp/1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Publication {
+    string str;
+    float price;
+public:
+    virtual void getdata() {
+        cout << "Enter string and price: ";
+        cin >> str >> price;
+    }
+    virtual void putdata() {
+        cout << "String: " << str << "\nPrice: " << price << '\n';
+    }
+};
+
+class Book : public Publication {
+    int page;
+public:
+    void getdata() {
+        Publication::getdata();
+        cout << "Enter pages: ";
+        cin >> page;
+    }
+    void putdata() {
+        Publication::putdata();
+        cout << "Pages: " << page << '\n';
+    }
+};
+
+class Tape : public Publication {
+    float time;
+public:
+    void getdata() {
+        Publication::getdata();
+        cout << "Enter time: ";
+        cin >> time;
+    }
+    void putdata() {
+        Publication::putdata();
+        cout << "Time: " << time << '\n';
+    }
+};
+
+int main() {
+    Publication* arr[100];
+    int n = 0;
+    char ch = 'y';
+    do {
+        cout << "Enter data for books or tape (b/t): ";
+        cin >> ch;
+        if(ch == 'b') {
+            arr[n] = new Book;
+        }
+        else if(ch == 't') {
+            arr[n] = new Tape;
+        }
+        arr[n]->getdata();
+        arr[n]->putdata();
+        n++;
+        cout << "Continue? (y/n): ";
+        cin >> ch;
+    }
+    while(ch != 'n');
+    return 0;
+}
