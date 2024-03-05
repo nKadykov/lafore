6e731626505078e9eddd8a8c8201cd853282fa6c
nKadykov <nkadykov@gmail.com>
2024-03-05 19:11:07 +0300

    init

---
+++ 12/10.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <vector>
+
+class Fraction { // класс дроби
+    int dividend; // числитель
+    int divisor; // знаменатель
+public:
+    Fraction() {} // конструктор без аргументов
+    Fraction(int a, int b) : dividend(a), divisor(b) {} // конструктор с двумя аргументами
+    Fraction operator+(Fraction) const; // перегрузка оператора сложения
+    Fraction operator-(Fraction) const; // перегрузка оператора вычитания
+    Fraction operator*(Fraction) const; // перегрузка оператора умножения
+    Fraction operator/(Fraction) const; // перегрузка оператора деления
+    void putdata() { // метод получения данных от пользователя
+        int ch = '/';
+        std::cin >> dividend >> ch >> divisor;
+    }
+    void getdata() { // метод вывода данных на экран
+        std::cout << "Result: " << dividend << '/' << divisor << '\n';
+    }
+    Fraction lowterms(); // метод сокращения дроби
+    friend std::istream& operator>>(std::istream& s, Fraction& f); // перегрузка оператора извлечения
+    friend std::ostream& operator<<(std::ostream& s, Fraction& f); // перегрузка оператора вставки
+};
+
+int main() {
+    Fraction s1(2, 5), s2(1, 6), result;
+    char ch = 'y';
+    result = s1 + s2; // демонстрация операции сложения
+    result.getdata();
+    result = s1 - s2; // демонстрация операции вычитания
+    result.getdata();
+    result = s1 * s2; // демонстрация операции умножения
+    result.getdata();
+    result = s1 / s2; // демонстрация операции деления
+    result.getdata();
+    Fraction f;
+    std::cin >> f; // демонстрация операции извлечения
+    std::cout << "Your fraction: ";
+    std::cout << f; // демонстрация операции вставки
+    return 0;
+}
+
+Fraction Fraction::operator+(Fraction f) const {
+    int a = dividend * f.divisor + divisor * f.dividend;
+    int b = divisor * f.divisor;
+    return Fraction(a, b).lowterms();
+}
+
+Fraction Fraction::operator-(Fraction f) const {
+    int a = dividend * f.divisor - divisor * f.dividend;
+    int b = divisor * f.divisor;
+    return Fraction(a, b).lowterms();
+}
+
+Fraction Fraction::operator*(Fraction f) const {
+    int a = dividend * f.dividend;
+    int b = divisor * f.divisor;
+    return Fraction(a, b).lowterms();
+}
+
+Fraction Fraction::operator/(Fraction f) const {
+    int a = dividend * f.divisor;
+    int b = divisor * f.dividend;
+    return Fraction(a, b).lowterms();
+}
+
+Fraction Fraction::lowterms() {
+    long tnum, tden, temp, gcd;
+    tnum = labs(dividend);
+    tden = labs(divisor);
+    if(tden == 0) {
+        std::cout << "Incorrect divisor!";
+    }
+    else if(tnum == 0) {
+        dividend = 0;
+        divisor = 1;
+        return Fraction(dividend, divisor);
+    }
+    while(tnum != 0) {
+        if(tnum < tden) {
+            temp = tnum;
+            tnum = tden;
+            tden = temp;
+        }
+        tnum = tnum - tden;
+    }
+    gcd = tden;
+    dividend = dividend / gcd;
+    divisor = divisor / gcd;
+    return Fraction(dividend, divisor);
+}
+
+std::istream& operator>>(std::istream& s, Fraction& f) {
+    char ch = '/';
+    while(true) { // бесконечный цикл
+        std::cout << "Enter fraction: ";
+        s >> f.dividend;
+        if(!s.good()) { // проверка, является ли числитель числом
+            s.clear();
+            std::cout << "Incorrect dividend\n";
+            s.ignore(80, '\n');
+            continue;
+        }
+        s >> ch;
+        if(ch != '/') { // проверка, является ли символ '/'
+            std::cout << "Incorrect fraction\n";
+            s.ignore(80, '\n');
+            continue;
+        }
+        s >> f.divisor;
+        if(!s.good()) { // проверка, является ли знаменатель числом
+            s.clear();
+            std::cout << "Incorrect divisor\n";
+            s.ignore(80, '\n');
+            continue;
+        }
+        if(f.divisor == 0) { // проверка, является ли знаменатель не 0
+            std::cout << "Divisor can't be 0\n";
+            s.ignore(80, '\n');
+            continue;
+        }
+        else { // выйти из цикла, если все условия соблюдены
+            break;
+        }
+    }
+    return s;
+}
+
+std::ostream& operator<<(std::ostream& s, Fraction& f) {
+    s << f.dividend << '/' << f.divisor;
+    return s;
+}
