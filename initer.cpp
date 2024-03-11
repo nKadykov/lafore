d534cd846778ed3a19f09309edd2d84a8019d437
nKadykov <nkadykov@gmail.com>
2024-03-11 13:34:56 +0300

    init

--- initer.cpp
+++ initer.cpp
@@ -10,7 +10,6 @@ int main() {
     istream_iterator<float> cin_iter(cin);
     istream_iterator<float> end_of_stream;
     copy(cin_iter, end_of_stream, fList.begin());
-    cout << '\n';
     ostream_iterator<float> ositer(cout, "--");
     copy(fList.begin(), fList.end(), ositer);
     return 0;
